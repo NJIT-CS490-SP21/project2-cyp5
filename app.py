11942a0cf0a963f68e04414b8002d1b8addec89d
Chirag Patel <cyp5@njit.edu>
2021-03-12 19:33:43 +0000

    Made changes according to pylint requirements and now the pylint score is 10/10

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+"""
+Server side of the app
+"""
 import os
-from flask import Flask, send_from_directory, json, session
+from flask import Flask, send_from_directory, json
 from flask_socketio import SocketIO
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
@@ -14,15 +17,15 @@ APP.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 # Gets rid of a warning
 APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(APP)
+DB = SQLAlchemy(APP)
 # IMPORTANT: This must be AFTER creating db variable to prevent
 # circular import issues
 import models # pylint: disable=C0413
-db.create_all()
+DB.create_all()
 
-cors = CORS(APP, resources={r"/*": {"origins": "*"}})
-names = []
-socketio = SocketIO(
+CORS = CORS(APP, resources={r"/*": {"origins": "*"}})
+NAMES = []
+SOCKETIO = SocketIO(
     APP,
     cors_allowed_origins="*",
     json=json,
@@ -39,34 +42,34 @@ def index(filename):
     return send_from_directory('./build', filename)
 
 # When a client connects from this Socket connection, this function is run
-@socketio.on('connect')
+@SOCKETIO.on('connect')
 def on_connect():
     """ Connects with client"""
     print('User connected!')
 
-@socketio.on('ticTac')
-def ticTac(data):
+@SOCKETIO.on('ticTac')
+def tic_tac(data):
     """ data is whatever arg you pass in your emit call on client
     This emits the 'ticTac' event from the server to all clients except for
     the client that emmitted the event that triggered this function"""
-    socketio.emit('ticTac', data, broadcast=True, include_self=False)
+    SOCKETIO.emit('ticTac', data, broadcast=True, include_self=False)
 
-@socketio.on('user')
+@SOCKETIO.on('user')
 def user(data):
     """ data is whatever arg you pass in your emit call on client"""
-    names.append(data['name'])
+    NAMES.append(data['name'])
     # This emits the 'user' event from the server to all clients except for
     # the client that emmitted the event that triggered this function
-    socketio.emit('user', names, broadcast=True, include_self=True)
+    SOCKETIO.emit('user', NAMES, broadcast=True, include_self=True)
     score_board = []
     score = []
-    exists = db.session.query(models.Person.username).filter_by(username=data['name']).first()# pylint: disable=E1101
+    exists = DB.session.query(models.Person.username).filter_by(username=data['name']).first()# pylint: disable=E1101
     if not exists:
         new_user = models.Person(username=data['name'], score=100)
-        db.session.add(new_user)# pylint: disable=E1101
-        db.session.commit()# pylint: disable=E1101
+        DB.session.add(new_user)# pylint: disable=E1101
+        DB.session.commit()# pylint: disable=E1101
 
-    query_object = db.session.query(models.Person)# pylint: disable=E1101
+    query_object = DB.session.query(models.Person)# pylint: disable=E1101
     descending_order = sqlalchemy.sql.expression.desc(models.Person.score)
     query_by_order = query_object.order_by(descending_order)
 
@@ -74,23 +77,23 @@ def user(data):
         score_board.append(person.username)
         score.append(person.score)
 
-    socketio.emit('score_board', {'users': score_board})
-    socketio.emit('score', {'score': score})
+    SOCKETIO.emit('score_board', {'users': score_board})
+    SOCKETIO.emit('score', {'score': score})
 
-@socketio.on('results')
+@SOCKETIO.on('results')
 def results(data):
     """ Adds +1 to winner score and -1 to losers score and arranges it in descending order"""
     score_board = []
     score = []
-    outcome1 = db.session.query(models.Person).filter_by(username=data['win']).first()# pylint: disable=E1101
+    outcome1 = DB.session.query(models.Person).filter_by(username=data['win']).first()# pylint: disable=E1101
     outcome1.score = outcome1.score + 1
     print(outcome1.score)
-    outcome2 = db.session.query(models.Person).filter_by(username=data['lose']).first()# pylint: disable=E1101
+    outcome2 = DB.session.query(models.Person).filter_by(username=data['lose']).first()# pylint: disable=E1101
     outcome2.score = outcome2.score - 1
     print(outcome2.score)
-    db.session.commit()# pylint: disable=E1101
+    DB.session.commit()# pylint: disable=E1101
 
-    query_object = db.session.query(models.Person)# pylint: disable=E1101
+    query_object = DB.session.query(models.Person)# pylint: disable=E1101
     descending_order = sqlalchemy.sql.expression.desc(models.Person.score)
     query_by_order = query_object.order_by(descending_order)
 
@@ -98,27 +101,27 @@ def results(data):
         score_board.append(person.username)
         score.append(person.score)
 
-    socketio.emit('score_board', {'users': score_board})
-    socketio.emit('score', {'score': score})
+    SOCKETIO.emit('score_board', {'users': score_board})
+    SOCKETIO.emit('score', {'score': score})
 
-@socketio.on('remove_user')
+@SOCKETIO.on('remove_user')
 def remove_user(data):
     """data is whatever arg you pass in your emit call on client"""
-    names.remove(data)
-    print(names)
+    NAMES.remove(data)
+    print(NAMES)
     # This emits the 'remove_user' event from the server to all clients except for
     # the client that emmitted the event that triggered this function
-    socketio.emit('remove_user', names, broadcast=True, include_self=True)
+    SOCKETIO.emit('remove_user', NAMES, broadcast=True, include_self=True)
 
 # When a client disconnects from this Socket connection, this function is run
-@socketio.on('disconnect')
+@SOCKETIO.on('disconnect')
 def on_disconnect():
     """ When a user disconnets prints the following message"""
     print('User disconnected!')
 # Note that we don't call app.run anymore. We call socketio.run with app arg
 if __name__ == "__main__":
 # Note that we don't call app.run anymore. We call socketio.run with app arg
-    socketio.run(
+    SOCKETIO.run(
         APP,
         host=os.getenv('IP', '0.0.0.0'),
         port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),# pylint: disable=W1508
--- models.py
+++ models.py
@@ -1,8 +1,8 @@
-from app import db
+from app import DB
 
-class Person(db.Model):
-    username = db.Column(db.String(80), primary_key=True)
-    score = db.Column(db.Integer, unique=False, nullable=False)
+class Person(DB.Model):
+    username = DB.Column(DB.String(80), primary_key=True)
+    score = DB.Column(DB.Integer, unique=False, nullable=False)
 
     def __repr__(self):
         return '<Person %r>' % self.username
